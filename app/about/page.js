7fb6da6a65f58bdb9a5fb7cbcc687e0f9a9033c3
aswinss18 <aswinartography18@gmail.com>
2024-10-21 10:46:12 +0530

    the basic file structure is craeted and routung is created

---
+++ app/about/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+
+export default function page() {
+  return (
+    <div>
+      <Navigation />
+      <h1>About me</h1>
+    </div>
+  );
+}
