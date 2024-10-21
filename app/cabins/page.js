428aa0eb7a025bae48ba87c6483ca92a109e1923
aswinss18 <aswinartography18@gmail.com>
2024-10-21 17:38:08 +0530

    Global Loader is craeted

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Navigation from "../components/Navigation";
+import Counter from "../components/Counter";
 
 export default async function page() {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
@@ -13,6 +14,7 @@ export default async function page() {
           <li key={user.id}>{user.name}</li>
         ))}
       </ul>
+      <Counter />
     </div>
   );
 }
