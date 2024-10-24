e7fbd1ee9d98b575d0461e813386ded5cac5557a
aswinss18 <aswinartography18@gmail.com>
2024-10-24 19:59:59 +0530

    Custom tailwind configuration for colors is added

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,20 +1,10 @@
 import React from "react";
-import Navigation from "../components/Navigation";
-import Counter from "../components/Counter";
 
-export default async function page() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  const data = await res.json();
+export default function page() {
   console.log(data);
   return (
     <div>
       <h1>Cabins</h1>
-      <ul>
-        {data.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
-      <Counter />
     </div>
   );
 }
--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,13 @@
 import React from "react";
-import Navigation from "./components/Navigation";
-import Logo from "./components/Logo";
+import Navigation from "./_components/Navigation";
+import Logo from "./_components/Logo";
+import "@/app/_styles/globals.css";
+
 export const metadata = { title: "The Wild Oasis " };
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-primary-950 text-primary-100 min-h-screen">
         <header>
           <Logo />
         </header>
