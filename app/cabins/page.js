1413b10f9a483f15e56b6ff99f6f74b87e140989
aswinss18 <aswinartography18@gmail.com>
2024-10-31 10:53:22 +0530

    Created a route end point for test

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -3,6 +3,7 @@ import CabinList from "../_components/CabinList";
 import Spinner from "../_components/Spinner";
 import Counter from "../_components/Counter";
 import Filter from "../_components/Filter";
+import ReservationReminder from "../_components/ReservationReminder";
 
 export const revalidate = 3600;
 // export const revalidate = 15;
@@ -11,8 +12,8 @@ export const metadata = {
   title: "Cabins",
 };
 
-export default function Page({searchParams}) {
-const filter=searchParams?.capacity??"all"
+export default function Page({ searchParams }) {
+  const filter = searchParams?.capacity ?? "all";
 
   return (
     <div>
@@ -27,12 +28,14 @@ const filter=searchParams?.capacity??"all"
         home away from home. The perfect spot for a peaceful, calm vacation.
         Welcome to paradise.
       </p>
-      <div>
-        <Filter/>
+
+      <div className="flex justify-end mb-8">
+        <Filter />
       </div>
 
       <Suspense fallback={<Spinner />} key={filter}>
         <CabinList filter={filter} />
+        <ReservationReminder />
       </Suspense>
     </div>
   );
