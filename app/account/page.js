280eb8cdad9a21b3f8859fa54ad086894eceb745
aswinss18 <aswinartography18@gmail.com>
2024-11-01 07:17:15 +0530

    Signin and Signout functions are created

--- app/account/page.js
+++ app/account/page.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { auth } from "../_lib/auth";
 
 export const metadata = { title: "Account" };
-export default function page() {
+export default async function page() {
+const session=await auth()
+
   return (
     <div>
       <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-        Welcome, Aswin
+        Welcome, {session.user.name.split(" ").at(0)}
       </h2>
     </div>
   );
